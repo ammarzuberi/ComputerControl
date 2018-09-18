d378f10876b3fad0838594ca1ca40d9545169cf0
Ammar Zuberi <zuberiammar@gmail.com>
2018-09-18 02:29:14 -0400

    Deploy to development server

--- server/server.go
+++ server/server.go
@@ -5,7 +5,7 @@ import (
 )
 
 var Applications = map[string]interface{}{
-	"/echo/helloworld": alexa.EchoApplication{ // Route
+	"/": alexa.EchoApplication{ // Route
 		AppID:    "123455", // Echo App ID from Amazon Dashboard
 		OnIntent: EchoIntentHandler,
 		OnLaunch: EchoIntentHandler,
