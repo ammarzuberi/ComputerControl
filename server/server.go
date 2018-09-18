113fc411d6d04c846d5ceffca2b442f9e7975422
Ammar Zuberi <zuberiammar@gmail.com>
2018-09-18 02:47:27 -0400

    Deploy to development server

--- server/server.go
+++ server/server.go
@@ -2,18 +2,28 @@ package main
 
 import (
 	alexa "github.com/mikeflynn/go-alexa/skillserver"
+	"log"
+	"net/http"
 )
 
-var Applications = map[string]interface{}{
-	"/echo/helloworld": alexa.EchoApplication{ // Route
-		AppID:    "amzn1.ask.skill.058881dc-31d8-49a8-bf02-24684d82b9c0", // Echo App ID from Amazon Dashboard
-		OnIntent: EchoIntentHandler,
-		OnLaunch: EchoIntentHandler,
+var skills = map[string]interface{}{
+	"/echo/computercontrol": alexa.EchoApplication{ // Route
+		AppID:   "amzn1.ask.skill.058881dc-31d8-49a8-bf02-24684d82b9c0", // Echo App ID from Amazon Dashboard
+		Handler: echoHandleIntent,
 	},
 }
 
 func main() {
-	alexa.Run(Applications, "3000")
+	alexa.Run(skills, "3000")
+}
+
+func echoHandleIntent(w http.ResponseWriter, r *http.Request) {
+	req := alexa.GetEchoRequest(r)
+
+	switch req.GetIntentName() {
+	case "lock":
+		log.Println("Locking computer")
+	}
 }
 
 func EchoIntentHandler(echoReq *alexa.EchoRequest, echoResp *alexa.EchoResponse) {
